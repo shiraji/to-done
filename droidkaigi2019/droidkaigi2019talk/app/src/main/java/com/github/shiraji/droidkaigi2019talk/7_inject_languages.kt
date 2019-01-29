ecee836e7d13efdb46cd83af29cd55f98896ba3c
shiraji <isogai.shiraji@gmail.com>
2019-01-30 02:12:07 +0900

    update

---
+++ droidkaigi2019/droidkaigi2019talk/app/src/main/java/com/github/shiraji/droidkaigi2019talk/7_inject_languages.kt
@@ -0,0 +1,13 @@
+package com.github.shiraji.droidkaigi2019talk
+
+// JSON書きやすくする
+
+fun fooJson() {
+    """
+    {
+      "name": "shiraji",
+      "github": "@shiraji",
+      "age": 35
+    }
+    """.trimIndent()
+}
