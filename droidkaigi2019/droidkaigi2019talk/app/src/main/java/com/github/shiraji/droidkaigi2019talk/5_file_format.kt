0bbbfc1f00ed1d835897e55a005ff4439b8833e5
shiraji <isogai.shiraji@gmail.com>
2019-01-30 03:31:35 +0900

    foo

--- droidkaigi2019/droidkaigi2019talk/app/src/main/java/com/github/shiraji/droidkaigi2019talk/5_file_format.kt
+++ droidkaigi2019/droidkaigi2019talk/app/src/main/java/com/github/shiraji/droidkaigi2019talk/5_file_format.kt
@@ -2,8 +2,8 @@ package com.github.shiraji.droidkaigi2019talk
 
 // Format忘れ防止
 
-fun format(bar: Int) {
-    if(bar != 5){
-    println(bar)
-    }
-}
+//fun format(bar: Int) {
+//    if(bar != 5){
+//    println(bar)
+//    }
+//}
