733d182dc2f29d8e9892f2b57093096e2ac8b844
shiraji <isogai.shiraji@gmail.com>
2019-01-31 01:34:24 +0900

    update this

---
+++ droidkaigi2019/droidkaigi2019talk/app/src/main/java/com/github/shiraji/droidkaigi2019talk/12_maximum_editor.kt
@@ -0,0 +1,11 @@
+package com.github.shiraji.droidkaigi2019talk
+
+// Android Studio can be text editor!!!
+
+// hide tool windows
+// none tabs placement
+// breadcrumbs
+// show line numbers
+// Gutter icons
+// Status bar
+// navigation bar
