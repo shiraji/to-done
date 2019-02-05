e733c8cf12c7e8903d0621dd1bdb03288f223d67
shiraji <isogai.shiraji@gmail.com>
2019-02-06 02:24:35 +0900

    update this

--- droidkaigi2019/droidkaigi2019talk/app/src/main/java/com/github/shiraji/droidkaigi2019talk/7_inject_languages.kt
+++ droidkaigi2019/droidkaigi2019talk/app/src/main/java/com/github/shiraji/droidkaigi2019talk/7_inject_languages.kt
@@ -1,12 +1,14 @@
 package com.github.shiraji.droidkaigi2019talk
 
+import org.intellij.lang.annotations.Language
+
 // JSON書きやすくする
 // Alt+Enter language=JSON
 // Preferences | Editor | Language Injections | Advanced
 
 fun fooJson() {
-    val raws = "{\n  \"name\": \"shiraji\",\n  \"github\": \"@shiraji\",\n  \"age\": 35\n}"
-    val json = "{\n  \"name\": \"shiraji\",\n  \"github\": \"@shiraji\",\n  \"age\": 35\n}"
+    val raws = "{\n  \"name\": \"shiraji\",\n  \"github\": \"@shiraji\"\n}"
+    val json = "{\n  \"name\": \"shiraji\",\n  \"github\": \"@shiraji\"\n}"
 }
 
 fun fooSQL() {
